00a92666822140919af9a75da1a7a6d58b4ea96d
Merovex <dausha@gmail.com>
2017-08-19 23:24:32 -0400

    Hasty Commit: 2017-08-19 23:24:31

--- main.js
+++ main.js
@@ -1,50 +1,36 @@
-const {
-	app,
-	BrowserWindow
-} = require('electron')
-const path = require('path')
-const url  = require('url')
-const windowStateKeeper = require('electron-window-state');
+"use strict";
+exports.__esModule = true;
+var electron_1 = require("electron");
+var path = require('path');
+var url = require('url');
+var windowStateKeeper = require('electron-window-state');
 require('electron-reload')(__dirname);
-
-let win;
-
+var win;
 function createWindow() {
-
-	let mainWindowState = windowStateKeeper({ defaultWidth: 1000, defaultHeight: 800 });
-
-	// Create Browser Window, using windowStateKeeper.
-	win = new BrowserWindow({
-		'x': mainWindowState.x,
-	    'y': mainWindowState.y,
-	    'width': mainWindowState.width,
-	    'height': mainWindowState.height
-	});
-	mainWindowState.manage(win);
-
- 	win.loadURL(`file://${__dirname}/index.html`) // load index.html
-
-	// open devtools
-	// win.webContents.openDevTools();
-
-console.log(app.getName())
-	win.on('closed', function() { win = null; })
+    var mainWindowState = windowStateKeeper({ defaultWidth: 1000, defaultHeight: 800 });
+    // Create Browser Window, using windowStateKeeper.
+    win = new electron_1.BrowserWindow({
+        'x': mainWindowState.x,
+        'y': mainWindowState.y,
+        'width': mainWindowState.width,
+        'height': mainWindowState.height
+    });
+    mainWindowState.manage(win);
+    win.loadURL("file://" + __dirname + "/index.html"); // load index.html
+    // open devtools
+    // win.webContents.openDevTools();
+    console.log(electron_1.app.getName());
+    win.on('closed', function () { win = null; });
 }
-
-app.on('ready', createWindow);
-
+electron_1.app.on('ready', createWindow);
 // Quit when all windows are closed
-app.on('window-all-closed', function() {
-
-	if (process.platform != 'darwin') {
-		app.quit();
-	}
-})
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+electron_1.app.on('window-all-closed', function () {
+    if (process.platform != 'darwin') {
+        electron_1.app.quit();
+    }
+});
+electron_1.app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    // if (mainWindow === null) { createWindow() }
+});
