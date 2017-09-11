2ecdd83102b0167c8c0b0888855d6054f566159f
Merovex <dausha@gmail.com>
2017-09-10 21:58:16 -0400

    Hasty Commit: 2017-09-10 21:58:15

---
+++ autumn-waterfall/src/components/project/Project.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Project</h1>
+  </div>
+</template>
+<script>
+  import ProjectDetail from './ProjectDetail.vue';
+  import ProjectEdit from './ProjectEdit.vue';
+  export default {
+    data: function() {
+      return {
+        name: 'Max'
+      ;}
+    },
+    methods: {
+
+    }
+  }
+</script>
+<style>
+
+</style>
